fd68e09e6a5e7f74224a1bb8d103a7c7b91728c2
LeydsonW <120534903+LeydsonW@users.noreply.github.com>
2025-05-10 23:26:49 -0300

    Base

--- src/data/navigation.ts
+++
@@ -1,8 +0,0 @@
-export const navigation = [
-  { name: "Home", path: "/" },
-  { name: "Pokémon", path: "/pokemons" },
-  { name: "Maps", path: "/maps" },
-  { name: "Quests", path: "/quests" },
-  { name: "Skins", path: "/skins" },
-  { name: "Systems", path: "/systems" }
-];
